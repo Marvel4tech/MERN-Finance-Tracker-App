dadd8a2c6463aac70da9e16f11b873a0fbcb6fe1
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-11-11 13:35:46 +0100

    form record values are added

--- client/src/pages/dashboard/financial-record-form.jsx
+++ client/src/pages/dashboard/financial-record-form.jsx
@@ -1,19 +1,61 @@
+import { useState } from "react"
+import { useUser } from "@clerk/clerk-react"
+
 export const FinancialRecordForm = () => {
+    const [description, setDescription] = useState("")
+    const [amount, setAmount] = useState("")
+    const [category, setCategory] = useState("")
+    const [paymentMethod, setPaymentMethod] = useState("")
+
+    const {user} = useUser()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const newRecord = {
+            userId: user?.id,
+            date: new Date(),
+            description: description,
+            amount: parseFloat(amount),
+            category: category,
+            paymentMethod: paymentMethod,
+        }
+
+        // addRecord(newRecord)
+        setDescription("")
+        setAmount("")
+        setCategory("")
+        setPaymentMethod("")
+    }
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label>Description:</label>
-                    <input type="text" required label='Description' />
+                    <input 
+                        type="text" 
+                        required 
+                        label='Description' 
+                        value={description} 
+                        onChange={(e) => setDescription(e.target.value)}
+                        className=" border"
+                    />
                 </div>
                 <div>
                     <label>Amount:</label>
-                    <input type="number" required label='Amount' />
+                    <input 
+                        type="number" 
+                        required 
+                        label='Amount' 
+                        value={amount} 
+                        onChange={(e) => setAmount(e.target.value)}
+                        className=" border"
+                    />
                 </div>
                 <div>
                     <label>Category:</label>
-                    <select required>
+                    <select required value={category} onChange={(e) => setCategory(e.target.value)} className=" border">
                         <option value="">Select a Category</option>
                         <option value="Food">Food</option>
                         <option value="Rent">Rent</option>
@@ -25,14 +67,14 @@ export const FinancialRecordForm = () => {
                 </div>
                 <div>
                     <label>Payment Method:</label>
-                    <select required>
+                    <select required value={paymentMethod} onChange={(e) => setPaymentMethod(e.target.value)} className=" border">
                         <option value="">Select a Payment Method</option>
                         <option value="Credit Card">Credit Card</option>
                         <option value="Bank">Bank</option>
                         <option value="Bank Transfer">Bank Transfer</option>
                     </select>
                 </div>
-                <button type="submit">
+                <button type="submit" className=" bg-green-600 py-2 px-4 rounded-md text-white">
                     Add Record
                 </button>
             </form>
