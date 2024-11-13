dac29134924461c5575a9714c7a81770cb4c54bc
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-11-13 10:54:16 +0100

    fetchRecords function (byUserID) API is added

--- client/src/contexts/financial-record-context.jsx
+++ client/src/contexts/financial-record-context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const FinancialRecordContext = createContext()
 
@@ -6,6 +7,22 @@ export const useFinancialRecord = () => useContext(FinancialRecordContext)
 
 export const FinancialRecordProvider = ({ children }) => {
     const [records, setRecords] = useState([])
+    const { user } = useUser()
+
+    const fetchRecords = async () => {
+        if(!user) return
+        const response = await fetch(`http://localhost:3000/financial-records/getAllByUserID/${user.id}`)
+
+        if (response.ok) {
+            const records = await response.json();
+            console.log(records)
+            setRecords(records);
+        }
+    }
+
+    useEffect(() => {
+        fetchRecords()
+    }, [user])
 
     const addRecord = async (record) => {
         const response = await fetch("http://localhost:3000/financial-records", {
